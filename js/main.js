d826a35cbeccaee5f4d3005c4ee877d85aaa6051
wunschpunsh <mnkaaaa@yandex.ru>
2021-10-06 17:49:23 +0300

    исправил рекомендации

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 function getRandomNumber (from, to) {
-  let range = to - from;
-  let randomNumber = Math.round(range * Math.random());
-  let result = from + randomNumber;
+  const range = to - from;
+  const randomNumber = Math.round(range * Math.random());
+  const result = from + randomNumber;
   return result;
 }
 
-function getRandomNumberWithPoints (from, to, point) {
-  let range = to - from;
-  let randomNumber = range * Math.random();
-  let result = from + randomNumber.toFixed(point);
+function getRandomFloatNumber (from, to, point) {
+  const range = to - from;
+  const randomNumber = range * Math.random();
+  const result = from + randomNumber.toFixed(point);
   return result;
 }
 
