c0f4b6839570f7f0a453d6d1909bac98af4005cf
wunschpunsh <mnkaaaa@yandex.ru>
2021-11-04 21:49:40 +0300

    начало 8 модуля

--- js/main.js
+++ js/main.js
@@ -1,2 +1,16 @@
+//import { similarGeneration } from './data/get-similar-generation.js';
+//import { createPopup } from './data/popup.js';
 import { setFormStateDisabled } from './form/form.js';
-setFormStateDisabled(true);
+/*const mapCanvas = document.querySelector('.map__canvas');
+const similarCardFragment = document.createDocumentFragment();
+similarGeneration.forEach((object) => {
+  const popup = createPopup(object);
+  similarCardFragment.append(popup);
+});
+
+mapCanvas.append(similarCardFragment.children[0]);
+*/
+setFormStateDisabled(false);
+const priceHouse = document.querySelector('input[type="number"]');
+const typeHouses = document.querySelectorAll('select[name="type"]');
+
