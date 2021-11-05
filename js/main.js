4b2c67325cc35ed28348f28e2532cb7f27e62bb7
wunschpunsh <mnkaaaa@yandex.ru>
2021-11-05 22:28:08 +0300

    добавил обработчик событий на type и price

--- js/main.js
+++ js/main.js
@@ -11,6 +11,44 @@ similarGeneration.forEach((object) => {
 mapCanvas.append(similarCardFragment.children[0]);
 */
 setFormStateDisabled(false);
-const priceHouse = document.querySelector('input[type="number"]');
-const typeHouses = document.querySelectorAll('select[name="type"]');
 
+const price = document.querySelector('input[name="price"]');
+const type = document.querySelector('select[name="type"]');
+//const room = document.querySelector('select[name="rooms"]');
+//const campacity = document.querySelector('select[name="capacity"]');
+const typeToValue = {
+  bungalow: 0,
+  flat: 1000,
+  hotel: 3000,
+  house: 5000,
+  palace: 10000,
+};
+const typeToPlaceholder = {
+  bungalow: '0',
+  flat: '1000',
+  hotel: '3000',
+  house: '5000',
+  palace: '10000',
+
+};
+/*const roomValue = {
+  1: 3,
+  2: 2,
+  3: 1,
+  100: 0,
+};*/
+
+type.addEventListener('change', () => {
+  price.min = typeToValue[type.value];
+  price.placeholder = typeToPlaceholder[type.value];
+});
+
+/*room.addEventListener('change', () => {
+  if (campacity.value !== roomValue[room.value]) {
+    campacity.remove(campacity.value);
+  } else {
+    campacity.value = roomValue[room.value]
+
+  }
+
+});*/
